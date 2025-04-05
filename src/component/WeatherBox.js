95a56e0f909fdb4ce1be20ead87c73c8e08e9407
lliwIWant <liwil5486@gmail.com>
2025-04-06 01:46:56 +0900

    end!!

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -10,7 +10,7 @@ const WeatherBox = ({weather}) => {
         {weather?(<h2>Today's weather</h2>):(<h2>Oops!😢</h2>)}
         <br/>
         <h4>{weather?.name}</h4>
-        {weather?(<h3>{weather?.main.temp}°C /{(weather?.main.temp * 9/5 + 32).toFixed(1)}°F</h3>):(<div className='warn'> We couldn't get your location.<br/> Please check your browser settings and allow access.</div>)}
+        {weather?(<h3>{weather?.main.temp}°C /{(weather?.main.temp * 9/5 + 32).toFixed(1)}°F</h3>):(<div className='warn'>Please check your browser settings and allow access.</div>)}
         <div>{weather?.weather[0].description}</div>
     </div>
   )
